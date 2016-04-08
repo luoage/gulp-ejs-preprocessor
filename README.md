2062b43cba05913e73084d93d685acfe7db81c89
luoage <luoage@gmail.com>
2016-04-08 17:48:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+##gulp-ejs-preprocessor
+
+---
+
+	var gulp = require('gulp');
+
+	gulp.src('./template/**/*.ejs')
+		.pipe(buildEjs())
+		.pipe(gulp.dest('./build'));
+
+###Options
+
+- function, it juse have only one param providing data to ejs files, like:
+
+	gulp.src('./template/**/*.ejs')
+		.pipe(buildEjs(function(file, encoding){
+			return {};
+		}))
+		.pipe(gulp.dest('./build'));
---
+++ index.js
@@ -0,0 +1,31 @@
+var ejs = require('ejs'),
+	Transform = require('stream').Transform;
+
+var buildEjs = function(fn){
+	var transform = new Transform({objectMode:true});
+
+	fn = (fn instanceof Function) ? fn : function(){};
+
+	transform._transform = function(data, encoding, cb){
+		var content = (data.contents || '').toString(encoding),
+			obj = fn(data, encoding);
+
+		try{
+			data.contents = new Buffer(ejs.render(content, obj || {}));
+		}catch(e){
+			return cb(e);
+		}
+		cb(null, data);
+	};
+
+	return transform;
+};
+
+//--------------
+
+var gulp = require('gulp');
+
+
+gulp.src('./template/**/*.ejs')
+	.pipe(buildEjs())
+	.pipe(gulp.dest('./build'));
